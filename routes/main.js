b9f6eda48e0fde2ca3460c0702e53ecef3f404b9
LeesMond <ljm71555555@gmail.com>
2022-11-24 02:51:55 +0000

    docker test

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
 const mongoClient = require('mongodb').MongoClient;
-const app = express;
+const app = express();
 
 app.use(morgan('dev'));
 
@@ -11,44 +11,46 @@ var db;
 var databaseUrl = "mongodb+srv://admin:1234@cluster0.5hijmxr.mongodb.net/?retryWrites=true&w=majority";
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + "/index.html");
-});
+    res.sendFile(__dirname + "/index.html");
+    });
 
-app.get('name-age', (req, res) => {
-	console.log(mongoClient)
-	mongoClient.connect(databaseUrl, function(err, database) {
-		if (err != null){
-			res.json({'count' : 0});
-			console.log(err);
-		} else {
-			db = database.db('test');
-			db.collection('test').find({}, {_id:0, id:1, name:1})
-			.toArray(function(err, result){
-				if (err) throw err;
-				console.log('result: ');
-				console.log(result);
-				//res.json(JSON.stringify(result));
+    app.get('id-name', (req, res) => {
+    console.log(mongoClient)
+    mongoClient.connect(databaseUrl, function(err, database) {
+    if (err != null) {
+    res.json({'count' : 0});
+    console.log(err);
+    } else {
+    db = database.db('test');
+    db.collection('things').find({}, {_id:0, id:1, name:1})
+    .toArray(function(err, result) {
+    if (err) throw err;
+    console.log('result: ');
+    console.log(result);
+    //res.json(JSON.stringify(result));
 
-				res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-				var template = ` 
-					<table border="1" margin: auto; text-align: center;>
-					<tr>
-						<th>아이디</th>
-						<th>이름</th>
-					</tr>
-				`;
-				result.forEach((item) => {
-					template += `
-						<tr>
-							<th>${item.id}</th>
-							<th>${item.name}</th>
-						</tr>`
-				});
-				template += `</table>`;
-				res.end(template);
-			});
-		}
-	});
-});
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    var template = `
+    <table border="1" margin: auto; text-align: center;>
+    <tr>
+    <th> 아이디 </th>
+    <th> 이름 </th>
+    </tr>
+    `;
 
-module.exports = app;
+    result.forEach((item) => {
+      	template += `
+	     	<tr>
+		    		<th>${item.id}</th>
+				       		<th>${item.name}</th>
+						       	</tr>`
+							});
+
+							template +=`</table>`;
+							res.end(template);
+							});
+							}
+							});
+							});
+
+							module.exports = app;
